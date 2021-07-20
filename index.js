92579ef553da49ec4a61576e0f5af8d98e76bd2c
Alexander Roldan <alexroldan49@gmail.com>
2021-07-20 11:39:52 -0400

    commit tree 4c4c431bccd69ac4554202e73f8bcfa652335a86

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}saturdayFun("bathe my dog")
+
+function mondayWork(act = 'go to the office') {
+    return `This Monday, I will ${act}.`
+}mondayWork()
+
+function wrapAdjective() {
+    return function (thing='special',){
+        return `you are${flair}${thing}${flair}`
+    }
+}
+wrapAdjective(Flair="*")
